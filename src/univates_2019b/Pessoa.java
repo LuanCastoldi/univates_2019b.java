eeeb582589cdd1fd643ff091e2fb253e9f684ea1
luan.castoldi <luan.castoldi@192.168.56.1>
2019-08-08 21:17:58 -0300

    Get e Set

---
+++ src/univates_2019b/Pessoa.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package univates_2019b;
+
+/**
+ *
+ * @author luan.castoldi
+ */
+public class Pessoa {
+    
+    private int codigo;
+    private String nome;
+    private String endereco;
+    private String telefone;
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+    
+    @Override
+    public String toString(){
+        return "Nome = " + this.nome;
+    }
+}
+
+
